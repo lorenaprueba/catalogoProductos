f302d077b7f57745c0dad50a64d14d43af55af64
Lorena Restrepo <lorenarestrepo@Lorenas-MacBook-Air.local>
2026-04-08 09:36:38 -0500

    Actualizar pantallas y diseño del menú lateral

--- app/src/main/java/com/example/catalogoproductos/ui/VideoFragment.kt
+++ app/src/main/java/com/example/catalogoproductos/ui/VideoFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.catalogoproductos.R
 
-class HomeFragment : Fragment() {
+class VideoFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_video, container, false)
     }
 }
