b88fa279f35d70e1d193878098953b550d699d61
dav0405 <dalmonacidgarnica@gmail.com>
2026-04-14 18:08:49 -0500

    Add files via upload

--- app/src/main/java/com/example/catalogoproductos/MainActivity.kt
+++ app/src/main/java/com/example/catalogoproductos/MainActivity.kt
@@ -20,5 +20,36 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         
         binding.navigationRail.setupWithNavController(navController)
+
+        // Forzar navegación manual si es necesario (Opcional pero recomendado para depurar)
+        binding.navigationRail.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.inicioFragment -> {
+                    navController.navigate(R.id.inicioFragment)
+                    true
+                }
+                R.id.catalogoFragment -> {
+                    navController.navigate(R.id.catalogoFragment)
+                    true
+                }
+                R.id.favoritosFragment -> {
+                    navController.navigate(R.id.favoritosFragment)
+                    true
+                }
+                R.id.carritoFragment -> {
+                    navController.navigate(R.id.carritoFragment)
+                    true
+                }
+                R.id.perfilFragment -> {
+                    navController.navigate(R.id.perfilFragment)
+                    true
+                }
+                R.id.webFragment -> {
+                    navController.navigate(R.id.webFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
